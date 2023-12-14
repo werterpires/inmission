d92915d76bf3fa4ccc5e30304a001f4b1789db08
werterpires <werterpires23@hotmail.com>
2023-12-14 11:25:49 -0300

    adicionando estilos para telas <480

--- in-mission/src/app/components/shared/menu-superior/menu-superior.component.ts
+++ in-mission/src/app/components/shared/menu-superior/menu-superior.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { NgClass, NgIf, isPlatformBrowser } from '@angular/common';
+import {
+  Component,
+  HostListener,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-menu-superior',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgClass, NgIf],
   templateUrl: './menu-superior.component.html',
   styleUrl: './menu-superior.component.css',
 })
-export class MenuSuperiorComponent {}
+export class MenuSuperiorComponent {
+  janelaMenor480 = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkScreenWidth();
+    }
+  }
+
+  checkScreenWidth() {
+    if (window.innerWidth < 480) {
+      this.janelaMenor480 = true;
+    } else {
+      this.janelaMenor480 = false;
+    }
+  }
+}
